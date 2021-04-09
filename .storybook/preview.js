2b144c2a56c1b649b9b7818e67cad6fb6488f377
joanne.cheng <supreme.evergreen@gmail.com>
2021-04-09 19:00:11 +0800

    Story Book Init

---
+++ .storybook/preview.js
@@ -0,0 +1,14 @@
+import '../src/index.css'
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+}
+
+// export const parameters = {
+//   actions: { argTypesRegex: "^on[A-Z].*" },
+//   controls: {
+//     matchers: {
+//       color: /(background|color)$/i,
+//       date: /Date$/,
+//     },
+//   },
+// }
